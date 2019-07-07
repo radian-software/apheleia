73c0a3b2194f138a55943074373a8ff3da59e41f
Radon Rosborough <radon.neon@gmail.com>
2019-07-07 14:13:57 -0700

    Add Elisp project skeleton

---
+++ apheleia.el
@@ -0,0 +1,39 @@
+;;; apheleia.el --- Reformat buffer stably -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Radon Rosborough
+
+;; Author: Radon Rosborough <radon.neon@gmail.com>
+;; Created: 7 Jul 2019
+;; Homepage: https://github.com/raxod502/apheleia
+;; Keywords: tools
+;; Package-Requires: ((emacs "25.2"))
+;; Version: 0
+
+;;; Commentary:
+
+;; Apheleia is an Emacs Lisp package which allows you to reformat a
+;; buffer without moving point. This solves the usual problem of
+;; running a tool like Prettier or Black on `before-save-hook', namely
+;; that it resets point to the beginning of the buffer. Apheleia
+;; maintains the position of point relative to its surrounding text
+;; even if the buffer is modified by the reformatting.
+
+;; Please see https://github.com/raxod502/apheleia for more information.
+
+;;; Code:
+
+(defgroup apheleia nil
+  "Better mode lighter overriding."
+  :group 'external
+  :link '(url-link :tag "GitHub" "https://github.com/raxod502/apheleia")
+  :link '(emacs-commentary-link :tag "Commentary" "apheleia"))
+
+;; TODO
+
+(provide 'apheleia)
+
+;; Local Variables:
+;; outline-regexp: ";;;;* "
+;; End:
+
+;;; apheleia.el ends here
