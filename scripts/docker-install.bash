25eae02054e4df2252e922cf650c2bf69bcc8e8e
Sifus <git@sifus.org>
2021-01-19 13:21:38 -0700

    Add formatters to Docker install script.

--- scripts/docker-install.bash
+++ scripts/docker-install.bash
@@ -10,8 +10,12 @@ fi
 
 uid="$1"
 
-packages="
+# Hashicorp repo for Terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | \
+    sudo tee /usr/share/keyrings/docker-ce-archive-keyring.gpg > /dev/null
+sudo apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
+apt_packages="
 # needed to run build system
 make
 
@@ -24,11 +28,53 @@ git
 # just in case we want root
 sudo
 
+# For formatters installable from opam
+ocaml
+opam
+
+# For formatters installable from npm
+nodejs
+npm
+
+# For formatters installable from pip
+python3
+python3-pip
+
+# Formatters installable from apt
+# latexindent
+texlive-extra-utils
+# gofmt
+golang-go
+terraform
+"
+
+opam_packages="
+ocamlformat
+"
+
+npm_packages="
+prettier
+"
+
+pip_packages="
+black
+"
+
+stack_packages="
+brittany
 "
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y $(grep -v "^#" <<< "$packages")
+apt-get install -y $(grep -v "^#" <<< "$apt_packages")
+opam install $(grep -v "^#" <<< "$opam_packages")
+npm install --global $(grep -v "^#" <<< "$npm_packages")
+pip install $(grep -v "^#" <<< "$pip_packages")
+# Stack
+curl -sSL https://get.haskellstack.org/ | sh
+stack install $(grep -v "^#" <<< "$stack_packages")
+
+
 rm -rf /var/lib/apt/lists/*
 
 useradd --uid="$uid" --create-home --groups sudo docker
