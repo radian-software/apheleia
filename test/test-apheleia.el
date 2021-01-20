749609076456402bae4271f63a7ef73e591463c4
Sifus <git@sifus.org>
2021-01-20 13:24:32 -0700

    Make test-apheleia.el work.
    Since the callback of apheleia--run-formatter is run in an asynchronous
    process, all output to stdout was being handled by the inferior process.
    Output must be retrieved in the main process with accept-process-output.
    
    Additionally, any formatter that uses the file directive will not run if
    buffer doesn't match the file on disk. with-temp-buffer doesn't have an
    associated file so most formatters wouldn't run.

--- test/test-apheleia.el
+++ test/test-apheleia.el
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-":"; exec emacs --script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
+:; exec emacs -Q -batch -script "$0" -- "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
 
 ;;; Usage: test-apheleia.el [OPTIONS] FILE MODE FORMATTER
 ;;; FILE is a path to a file to be formatted
@@ -18,15 +18,15 @@
 
 (defun get-file ()
   "Get and return FILE from command line arguments."
-  (elt argv 1))
+  (nth 1 argv))
 
 (defun get-mode ()
   "Get and return MODE from command line arguments."
-  (intern (elt argv 2)))
+  (intern (nth 2 argv)))
 
 (defun get-formatter ()
   "Get and return FORMATTER from command line arguments."
-  (intern (elt argv 3)))
+  (intern (nth 3 argv)))
 
 (defun validate-args ()
   "Validate command line arguments."
@@ -49,20 +49,12 @@
 
 (validate-args)
 
-;(setq file (get-file))
-;(setq mode (get-mode))
-;(setq formatter (get-formatter))
-
-
-(let* ((f "formatters/python-mode/black.py")
-       (mode 'python-mode)
-       (hoist t))
-  (with-temp-buffer
-    (insert-file-contents f)
-    (apheleia--run-formatter
-     (alist-get (alist-get mode apheleia-mode-alist)
-                apheleia-formatters)
-     (lambda (buf)
-       (with-current-buffer buf
-         ;(buffer-string)
-         (write-file "/tmp/klsdfkljd"))))))
+(let* ((file (get-file))
+       (mode (get-mode))
+       (formatter (get-formatter))
+       (proc
+        (with-current-buffer (find-file-noselect file)
+          (apheleia--run-formatter
+           (alist-get (alist-get mode apheleia-mode-alist) apheleia-formatters)
+           (lambda (buf) (with-current-buffer buf (princ (buffer-string))))))))
+  (while (accept-process-output proc)))
