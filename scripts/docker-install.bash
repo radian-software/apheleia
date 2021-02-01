f8a4aac346902388e9a6ab6af134d0796c535bae
Sifus <git@sifus.org>
2021-02-01 02:02:33 -0700

    Get formatter tests running in Docker.

--- scripts/docker-install.bash
+++ scripts/docker-install.bash
@@ -11,9 +11,16 @@ fi
 uid="$1"
 
 # Hashicorp repo for Terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | \
-    tee /usr/share/keyrings/docker-ce-archive-keyring.gpg > /dev/null
+apt-get update -y
+apt-get install software-properties-common -y
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor | \
+#    tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+curl -fsSL 'https://apt.releases.hashicorp.com/gpg' | apt-key add -
 apt-add-repository -y "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# NodeJS repo for up-to-date Node
+curl -fsSL 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key' | apt-key add -
+apt-add-repository -y "deb https://deb.nodesource.com/node_12.x $(lsb_release -cs) main"
 
 apt_packages="
 # needed to run build system
@@ -29,16 +36,21 @@ git
 sudo
 
 # For formatters installable from opam
-ocaml
-opam
+m4
 
 # For formatters installable from npm
 nodejs
-npm
 
 # For formatters installable from pip
 python3
 python3-pip
+
+# For formatters installable from stack
+libffi-dev
+libgmp-dev
+zlib1g-dev
+# For brittany
+libtinfo-dev
 
 # Formatters installable from apt
 # latexindent
@@ -48,28 +60,47 @@ golang-go
 terraform
 "
 
+# Avoid Opam if possible. Opam is source-based and may incur long build times.
+# Install binaries if possible.
 opam_packages="
 ocamlformat
 "
-
 npm_packages="
 prettier
 "
-
 pip_packages="
 black
 "
+# Avoid Stack if possible. Stack is source-based and may incur long build times.
+# Install binaries if possible.
+stack_packages="
+brittany
+"
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y $(grep -v "^#" <<< "$apt_packages")
-opam install $(grep -v "^#" <<< "$opam_packages")
+apt-get -y update
+apt-get -y install $(grep -v "^#" <<< "$apt_packages")
+
+# Install Ocaml and Opam
+curl -fsSL --output install.sh \
+    https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh
+chmod +x install.sh
+# Handle the unremoveable prompt from the install script
+echo "" | ./install.sh
+# Docker takes care of our sandboxing
+opam init --disable-sandboxing -y
+opam install --destdir=/usr/local -y $(grep -v "^#" <<< "$opam_packages")
+
 npm install --global $(grep -v "^#" <<< "$npm_packages")
-pip install $(grep -v "^#" <<< "$pip_packages")
-curl -L --output /usr/bin/brittany "$(curl -s \
-    https://api.github.com/repos/lspitzner/brittany/releases/latest | \
-    grep 'browser_' | cut -d\" -f4 | grep linux)"
-chmod +x /usr/bin/brittany
+
+pip3 install $(grep -v "^#" <<< "$pip_packages")
+
+# Install GHC and Stack
+export STACK_ROOT="/usr/local/.stack"
+curl -fsSL https://get.haskellstack.org/ | sh
+# Older resolver else Brittany fails to build
+stack --stack-root "$STACK_ROOT" install --local-bin-path /usr/local/bin \
+    --fast --resolver lts-16.25 $(grep -v "^#" <<< "$stack_packages")
 
 
 
