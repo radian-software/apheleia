1ddf662d0e3d862b8d744323d65ba96b8be383c9
dalu <moutong945@outlook.com>
2022-04-26 08:56:50 +0800

    fix: different architexture

--- test/formatters/installers/dart-format.bash
+++ test/formatters/installers/dart-format.bash
@@ -1,4 +1,18 @@
-wget "https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip"
+ARCH=$(uname -m)
+case $ARCH in
+    "x86_64")
+        ARCH="x64"
+        ;;
+    "i386")
+        ARCH="ia32"
+        ;;
+    "aarch64")
+        ARCH="arm64"
+esac
 
-unzip dartsdk-linux-x64-release.zip
-export PATH="$PATH:$(pwd)/dart-sdk/bin"
+mkdir /opt/dart && cd /opt/dart || exit
+
+wget "https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-$ARCH-release.zip"
+
+unzip ./dart*.zip
+echo "export PATH=\"$PATH:/opt/dart/dart-sdk/bin\"" >> /etc/bash.bashrc
