367d9705c2ce14d0296c670c616213235b83f7f9
Radon Rosborough <radon.neon@gmail.com>
2022-05-13 09:21:34 -0700

    Apply suggestions from code review

--- test/formatters/installers/ktlint.bash
+++ test/formatters/installers/ktlint.bash
@@ -1,4 +1,4 @@
-cd /usr/local/bin
 ver="$(latest_release pinterest/ktlint)"
-apt install -y default-jre
-curl -sSLO https://github.com/pinterest/ktlint/releases/download/${ver}/ktlint && chmod a+x ktlint
+apt-get install -y default-jre
+wget "https://github.com/pinterest/ktlint/releases/download/${ver}/ktlint" -O /usr/local/bin/ktlint
+chmod +x /usr/local/bin/ktlint
