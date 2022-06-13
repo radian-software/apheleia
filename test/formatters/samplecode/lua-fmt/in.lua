fe94620546e07f558a6dd9da2d2a9633fe839e7d
Kisaragi Hiu <mail@kisaragi-hiu.com>
2022-06-13 23:51:13 +0900

    Add lua-fmt

---
+++ test/formatters/samplecode/lua-fmt/in.lua
@@ -0,0 +1,19 @@
+   too_many_spaces     =        true
+
+local function foo    ()
+   return 1,
+      2,
+      3
+end
+
+local many, vars = function() return true end
+
+local function what_indent ()
+noindent  = true
+  if noindent  then
+     x = false
+  end
+      return x
+end
+
+local y = "Single quotes are used"
