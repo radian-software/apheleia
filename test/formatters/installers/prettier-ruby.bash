841aa02f12fed60eb487f8f3f1b8ff04a069015b
Radon Rosborough <radon.neon@gmail.com>
2022-06-20 15:51:55 -0700

    More

--- test/formatters/installers/prettier-ruby.bash
+++ test/formatters/installers/prettier-ruby.bash
@@ -1,2 +1,13 @@
-apt-get install -y ruby
-npm install -g prettier @prettier/plugin-ruby
+# Need ruby for gem, need gcc for native gem deps
+apt-get install -y ruby gcc
+
+# Need to do this manually for some reason because gem does not
+# apparently include a dependency solver that can recognize the older
+# version must be installed to support the Ruby version currently in
+# use.
+gem install syntax_tree -v 2.3.1
+
+# Despite the name this is actually installing the Prettier plugin for
+# Ruby, not Prettier itself. Also, same issue as above where we have
+# to specify the version manually.
+gem install prettier -v 2.1.0
