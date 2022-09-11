b653efb4da24051dc4a05edc2cb177f9050abf20
Radon Rosborough <radon@intuitiveexplanations.com>
2022-09-10 17:21:48 -0700

    release not tagged yet

--- test/formatters/installers/prettier-ruby.bash
+++ test/formatters/installers/prettier-ruby.bash
@@ -1,13 +1,14 @@
 # Need ruby for gem, need gcc and ruby headers for native gem deps
 apt-get install -y ruby ruby-dev gcc
 
-# Despite the name this is actually installing the Prettier plugin for
-# Ruby, not Prettier itself.
-gem install prettier
-
-# Then of course we need to install what sounds like the same thing
-# again, but isn't...
+# Install the plugin
 npm install -g prettier @prettier/plugin-ruby
+
+# Have to install from source because release not tagged yet
+# https://github.com/ruby-syntax-tree/syntax_tree-rbs/pull/34
+# https://stackoverflow.com/a/11767563
+gem install specific_install
+gem specific_install -l https://github.com/ruby-syntax-tree/syntax_tree-rbs.git
 
 # These are required dependencies documented at
 # https://www.npmjs.com/package/@prettier/plugin-ruby
