b13882915f730b6085e898a6fdd2e4d0174ed8c8
Abin Simon <mail@meain.io>
2022-12-06 09:55:11 +0530

    Add tests for gofumpt and goimports

---
+++ test/formatters/installers/goimports.bash
@@ -0,0 +1,3 @@
+apt-get install -y golang-go
+go get -u golang.org/x/tools/cmd/goimports
+cp -L "$HOME/go/bin/goimports" /usr/local/bin/
---
+++ test/formatters/samplecode/gofumpt/in.go
@@ -0,0 +1,10 @@
+package  asdfasdf
+
+func Factorial(x    int)     int {
+
+	if    x <= 1    {
+		   return x
+}
+	return x * Factorial(x   -  1,
+)
+    }
