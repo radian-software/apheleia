ca45e2056c0558e1fb62077901bad2ca09cd0d14
Mohsin Kaleem <mohkale@kisara.moe>
2023-03-12 11:39:42 +0000

    fixup! Add asmfmt formatter

--- test/formatters/installers/asmfmt.bash
+++ test/formatters/installers/asmfmt.bash
@@ -1,4 +1,6 @@
 version=1.3.2
 
-curl -L "https://github.com/klauspost/asmfmt/releases/download/v$version/asmfmt-Linux_x86_64_$version.tar.gz" |
-    tar -xvzC "/usr/local/bin/asmfmt"
+dest=$(mktemp -d)
+curl -L "https://github.com/klauspost/asmfmt/releases/download/v$version/asmfmt-Linux_x86_64_$version.tar.gz" | tar -xvzC "$dest"
+mv "$dest/asmfmt" /usr/local/bin/asmfmt
+rm -rf "$dest"
