c5c628f9491120cbda9c951cab8373c3aac63870
Mohsin Kaleem <mohkale@kisara.moe>
2023-04-20 12:32:45 +0100

    test: Change formatter

--- apheleia.el
+++ apheleia.el
@@ -32,7 +32,7 @@
   :link '(emacs-commentary-link :tag "Commentary" "apheleia"))
 
 (defcustom apheleia-formatters
-  '((bean-format . ("bean-format"))
+  '((bean-formatx . ("bean-format"))
     (black . ("black"
               (when (apheleia-formatters-extension-p "pyi") "--pyi")
               (apheleia-formatters-fill-column "--line-length")
