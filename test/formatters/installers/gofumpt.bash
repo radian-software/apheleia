35f72f6fbe42115202a43be61a07805b3854b0d2
Ed Slocomb <edslocomb@gmail.com>
2023-10-06 21:47:52 -0700

    use up-to-date golang (#218)
    While running a full `make fmt-build`, I got some failures with
    installer scripts for golang formatters. This fixes the problem by
    installing an up-to-date golang instead of the version available in
    ubuntu 20.04.
    
    The ppa used here, `longsleep/golang-backports` is
    [recommended](https://github.com/golang/go/wiki/Ubuntu) by the Go
    project.
    
    ---------
    
    Co-authored-by: Radon Rosborough <radon@intuitiveexplanations.com>

--- test/formatters/installers/gofumpt.bash
+++ test/formatters/installers/gofumpt.bash
@@ -1,6 +1,3 @@
-# Require at least version 1.17 of go
-# https://github.com/mvdan/gofumpt/issues/231
-curl -OL https://golang.org/dl/go1.19.3.linux-amd64.tar.gz
-sudo tar -C /usr/local -xvf go1.19.3.linux-amd64.tar.gz
-/usr/local/go/bin/go install mvdan.cc/gofumpt@latest
+apt-get install -y golang-go
+go install mvdan.cc/gofumpt@latest
 cp -L "$HOME/go/bin/gofumpt" /usr/local/bin/
