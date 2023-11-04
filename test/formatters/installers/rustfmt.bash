cdbdeb60c6158de350d02df2b8615b970e4c533d
Ed Slocomb <edslocomb@gmail.com>
2023-11-04 11:43:23 -0700

    try to get rustfmt test installer working both locally and on CI

--- test/formatters/installers/rustfmt.bash
+++ test/formatters/installers/rustfmt.bash
@@ -1,10 +1,10 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --profile minimal --component rustfmt -y
 
-mv $HOME/.cargo /usr/local/.cargo
-chmod -R a+r /usr/local/.cargo
-chmod -R a+x /usr/local/.cargo/bin
-mv $HOME/.rustup /usr/local/.rustup
-chmod -R a+r /usr/local/.rustup
-echo -e "\nln -s /usr/local/.rustup \$HOME/.rustup" >>/etc/bash.bashrc
-echo -e "\nln -s /usr/local/.cargo \$HOME/.cargo" >>/etc/bash.bashrc
+chmod -R a+rx $HOME
+chmod -R a+r $HOME/.cargo
+chmod -R a+x $HOME/.cargo/bin
+chmod -R a+r $HOME/.rustup
+echo -e "\nln -s $HOME/.rustup \$HOME/.rustup" >>/etc/bash.bashrc
+echo -e "ln -s $HOME/.cargo \$HOME/.cargo" >>/etc/bash.bashrc
 echo -e "source \$HOME/.cargo/env" >>/etc/bash.bashrc
+source $HOME/.cargo/env
