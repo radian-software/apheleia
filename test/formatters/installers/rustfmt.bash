27cb43e292cc281dad0951356fe831345c90a87c
Ed Slocomb <edslocomb@gmail.com>
2023-11-05 09:27:09 -0800

    add some debugging to figure out what's going wrong on CI

--- test/formatters/installers/rustfmt.bash
+++ test/formatters/installers/rustfmt.bash
@@ -1,10 +1,16 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain stable --profile minimal --component rustfmt -y
 
-chmod -R a+rx $HOME
+chmod a+rx $HOME
 chmod -R a+r $HOME/.cargo
-chmod -R a+x $HOME/.cargo/bin
-chmod -R a+r $HOME/.rustup
+chmod -R a+rx $HOME/.cargo/bin
+chmod a+x $HOME/.rustup
+chmod -R a+rw $HOME/.rustup
 echo -e "\nln -s $HOME/.rustup \$HOME/.rustup" >>/etc/bash.bashrc
 echo -e "ln -s $HOME/.cargo \$HOME/.cargo" >>/etc/bash.bashrc
 echo -e "source \$HOME/.cargo/env" >>/etc/bash.bashrc
 source $HOME/.cargo/env
+echo $PATH
+ls -l $HOME
+ls -l $HOME/.cargo
+ls -l $HOME/.rustup
+which rustup
