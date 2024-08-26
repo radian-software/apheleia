8184ebbbd6a4383e529d6372cbb340a547c90c2f
Dusan <dusanuveric@protonmail.com>
2024-08-26 11:13:37 +0200

    update rubocop command
    Signed-off-by: Dusan <dusanuveric@protonmail.com>

--- apheleia-formatters.el
+++ apheleia-formatters.el
@@ -158,7 +158,7 @@
     (python3-json
      . ("python3" "-m" "json.tool"
         (apheleia-formatters-indent "--tab" "--indent")))
-    (rubocop . ("rubocop" "--stdin" filepath "--auto-correct"
+    (rubocop . ("rubocop" "--stdin" filepath "--autocorrect"
                 "--stderr" "--format" "quiet" "--fail-level" "fatal"))
     (ruby-standard . ("standardrb" "--stdin" filepath "--fix" "--stderr"
                       "--format" "quiet" "--fail-level" "fatal"))
