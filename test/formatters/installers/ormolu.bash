50e5c857c576c56b2100ac139d787c64a7ea35c1
Radon Rosborough <radon@intuitiveexplanations.com>
2024-09-21 19:51:15 -0700

    Do not be idiot

--- test/formatters/installers/ormolu.bash
+++ test/formatters/installers/ormolu.bash
@@ -1,3 +1,3 @@
-wget https://github.com/tweag/ormolu/releases/download/0.7.7.0/ormolu-x86_64-linux.zip -O ormolu.zip
-unzip -nv ormolu.zip
+wget -nv https://github.com/tweag/ormolu/releases/download/0.7.7.0/ormolu-x86_64-linux.zip -O ormolu.zip
+unzip ormolu.zip
 cp ormolu /usr/local/bin/
