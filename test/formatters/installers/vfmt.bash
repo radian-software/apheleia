0ddb085aeacfa817ca661f9af781c6956fa7fc11
Radon Rosborough <radon@intuitiveexplanations.com>
2024-10-17 17:24:18 -0700

    fix

--- test/formatters/installers/vfmt.bash
+++ test/formatters/installers/vfmt.bash
@@ -1,9 +1,15 @@
 ver="$(latest_release vlang/v)"
 
-apt-get install -y gcc
-wget "https://github.com/vlang/v/releases/download/${ver}/v_linux.zip" -O v.zip
-unzip v.zip
-chmod a=u,go-w -R v
+apt-get install -y gcc libx11-dev
+
+wget -nv "https://github.com/vlang/v/releases/download/${ver}/v_linux.zip" -O v.zip
+unzip -q v.zip
+
 sudo mkdir /opt/vlang
 sudo cp -R v/. /opt/vlang/.
 sudo /opt/vlang/v symlink
+
+sudo v build-tools
+
+# thx https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=vlang
+sudo touch /opt/vlang/cmd/tools/.disable_autorecompilation
