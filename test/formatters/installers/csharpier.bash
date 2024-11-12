9c50eb8eaff712d4b2538d511ebe3500e5c6f0d6
Gabriel <172639817+gs-101@users.noreply.github.com>
2024-11-12 07:24:25 -0300

    Update csharpier.bash

--- test/formatters/installers/csharpier.bash
+++ test/formatters/installers/csharpier.bash
@@ -2,6 +2,7 @@ apt-get install -y dotnet-sdk-8.0
 
 dotnet tool install csharpier -g
 
-ls "${HOME}/.dotnet/tools" | while read -r tool; do
-    ln -s "${HOME}/.dotnet/tools/${tool}" /usr/local/bin/
-done
+if ! [[ "$PATH" =~ $HOME/.dotnet/tools:$HOME/bin: ]]; then
+    PATH="$HOME/.dotnet/tools:$HOME/bin:$PATH"
+fi
+export PATH
