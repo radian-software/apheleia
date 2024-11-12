5edc85d5ec5bf5f84470c8fef5095a7d720e27db
Radon Rosborough <radon@intuitiveexplanations.com>
2024-11-11 18:45:57 -0800

    Update csharpier.bash

--- test/formatters/installers/csharpier.bash
+++ test/formatters/installers/csharpier.bash
@@ -2,4 +2,4 @@ apt-get install -y dotnet-sdk-8.0
 
 dotnet tool install csharpier -g
 
-cp -L "$HOME/.dotnet/tools/dotnet-csharpier" /usr/local/bin/
+ln -s "$HOME/.dotnet/tools/dotnet-csharpier" /usr/local/bin/
