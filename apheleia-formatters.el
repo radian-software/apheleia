f2deba83802fff2fdcd2a65a2506c5a1835f3490
Ibrahim Awwal <ibrahim+github@awwal.dev>
2025-12-09 22:45:15 -0800

    test change to rubocop formatter

--- apheleia-formatters.el
+++ apheleia-formatters.el
@@ -180,7 +180,7 @@
     (python3-json
      . ("python3" "-m" "json.tool"
         (apheleia-formatters-indent "--tab" "--indent")))
-    (rubocop . ("rubocop" "--stdin" filepath "-a"
+    (rubocop . ("rubocop" "--stdin" filepath "-a" ;; test
                 "--stderr" "--format" "quiet" "--fail-level" "fatal"))
     (ruby-standard . ("standardrb" "--stdin" filepath "--fix" "--stderr"
                       "--format" "quiet" "--fail-level" "fatal"))
